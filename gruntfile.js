5dcb5f29455b47671f831520379b6f578205e30c
Christophe Brisbois <chr@brisbois.fr>
2016-11-17 00:23:29 +0100

    Create gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+  chmod: {
+    options: {
+      mode: '755'
+    },
+    yourTarget1: {
+      // Target-specific file/dir lists and/or options go here.
+      src: ['./*.js']
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-chmod');
+
+
+
+};
