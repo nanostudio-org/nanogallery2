3d361b168678b0dcae8f89b86725f0e33eb23580
Christophe Brisbois <chr@brisbois.fr>
2016-11-17 13:55:09 +0100

    Create gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+  var banner = [
+        '/* <%= pkg.name %> - v<%= pkg.version %> - ',
+        '<%= grunt.template.today("yyyy-mm-dd") %> - <%= pkg.homepage %> */\n'
+      ].join('');
+
+    grunt.initConfig({
+      concat: {
+        options: {
+          banner: banner
+        },
+        minimal: {
+          src: [
+            'header.html',
+            'test.html',
+            'footer.html'
+          ],
+          dest: 'mypage.html'
+        }
+      }
+      
+    grunt.registerTask('build-minimal', [
+      'concat:minimal'
+      /* 'uglify:standardTarget',
+      'concat:minimalDebug',
+      'yuidoc',
+      'copy:redirects' */
+    ]);
+}
+      
