307c47c169390664a418680ed341560de414fcc1
Christophe Brisbois <chr@brisbois.fr>
2016-12-21 23:33:36 +0100

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -3,7 +3,7 @@
 javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"2.0.2",function($,L){
   /* YOUR JQUERY CODE GOES HERE */
 
-  jQuery=$;
+  var jQuery=$;
 
 /**!
  * @preserve nanogallery2 - javascript image gallery
