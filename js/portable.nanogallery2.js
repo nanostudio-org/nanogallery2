5601d9f719488439f70bee15b0fcd111978b0182
Christophe Brisbois <chr@brisbois.fr>
2016-12-22 19:09:57 +0100

    Add files via upload

--- js/portable.nanogallery2.js
+++ js/portable.nanogallery2.js
@@ -3,7 +3,7 @@
 //
 javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"2.0.2",function($,L){
 var jQuery=$;
-
+var $=$;
 /**!
  * @preserve nanogallery2 - javascript image gallery
  * Homepage: http://nanogallery2.nanostudio.org
@@ -8382,6 +8382,7 @@ function makeArray( obj ) {
     // if ( $ ) {
     if ( jQuery ) {
       // add jQuery Deferred object
+      // this.jqDeferred = new $.Deferred();
       this.jqDeferred = new $.Deferred();
     }
 
