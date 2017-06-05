17823235087c3d12e5311a6a17e63b7805ff9f90
Kris-B <chr@brisbois.fr>
2017-06-05 00:09:22 +0000

    [ci skip]

--- dist/jquery.nanogallery2.js
+++ dist/jquery.nanogallery2.js
@@ -1495,6 +1495,7 @@ v1.4.0 - BETA VERSION - DO NOT USE
     this.Search = function( search ) {
       G.GOM.albumSearch=search.toUpperCase();
       GalleryRender( G.GOM.albumIdx );
+      alert(G.GOM.albumIdx);
       var albumID=G.I[G.GOM.albumIdx].GetID();
       var l=G.I.length;
       var cnt=0;
