50db3dc75a59a0d06dfe6d14dabc31aec653f5f4
Kris-B <chr@brisbois.fr>
2017-07-06 21:38:54 +0000

    [ci skip]

--- dist/jquery.nanogallery2.js
+++ dist/jquery.nanogallery2.js
@@ -1,4 +1,4 @@
-/* nanogallery2 - v1.4.0 - 2017-06-11 - http://nanogallery2.nanostudio.org */
+/* nanogallery2 - v1.4.0 - 2017-07-06 - http://nanogallery2.nanostudio.org */
 /**!
  * @preserve nanogallery2 - javascript image gallery
  * Homepage: http://nanogallery2.nanostudio.org
