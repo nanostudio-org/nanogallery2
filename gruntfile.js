e640380d1ec738cd1a68712ad611241df9a21cc3
Christophe Brisbois <chr@brisbois.fr>
2017-09-01 17:30:55 +0200

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -22,7 +22,9 @@ module.exports = function(grunt) {
             'template/header-2.html',
             'template/header-3.html',
             'template/logo_index.html',
+            'template/javascript.html',
             'src/index.html',
+            'src/index_js.html',
             'template/footer.html'
           ],
           dest: 'build/index.html'
