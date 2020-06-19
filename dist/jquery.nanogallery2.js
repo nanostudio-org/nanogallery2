d6320872c129534a6a4b8a3e11658638970d44e3
Kris-B <chr@brisbois.fr>
2020-06-19 14:05:02 +0000

    [ci skip]

--- dist/jquery.nanogallery2.js
+++ dist/jquery.nanogallery2.js
@@ -16700,7 +16700,7 @@ if (typeof define === 'function' && define.amdDISABLED) {
         PreloaderDisplay(false);
         
         // go through sourceData, and exclude blacklisted tags
-        sourceData = NGY2Tools.FilterByTags(sourceData, G.O.tagBlockList);
+        sourceData = FilterByTags(sourceData, G.O.tagBlockList);
 
         if( kind == 'album' ) {
           FlickrParsePhotoSets(albumIdx, albumID, sourceData);
