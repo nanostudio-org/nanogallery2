debd4ac928f1191d6ec3d08393e5c3b04085b357
Kris-B <chr@brisbois.fr>
2020-11-03 23:22:57 +0000

    [ci skip]

--- dist/jquery.nanogallery2.js
+++ dist/jquery.nanogallery2.js
@@ -1,4 +1,4 @@
-/* nanogallery2 - v3.0.4 - 2020-10-13 - https://nanogallery2.nanostudio.org */
+/* nanogallery2 - v3.0.4 - 2020-11-03 - https://nanogallery2.nanostudio.org */
 /*!
  * @preserve nanogallery2 - javascript photo / video gallery and lightbox
  * Homepage: http://nanogallery2.nanostudio.org
